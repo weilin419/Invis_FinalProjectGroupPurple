fc442a9d00df831b3ebd7782e2e10afdc8d86131
weilin419 <right_a_wei@163.com>
2025-11-17 01:16:49 +0000

    Rotation
    Use the “If Statement” to control objects rotation by GetKey.
    Set rotations by Vector3.
    Reset Rotation.
    Set Rotate to the Front, Back, Left and Right view.
    Control The Front View, Back View, Left View, Right View, and Reset Rotation by clicking on the UI buttons.

---
+++ Assets/Scripts/ManageRotation.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class ManageRotation : MonoBehaviour
+{
+    private float yRot = 0.0f;
+    private Vector3 eulerRot;
+
+    private Quaternion myInitialRotation;
+    private Quaternion frontRotation;
+    private Quaternion backRotation;
+    private Quaternion leftRotation;
+    private Quaternion rightRotation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Assign the original obj orientation to the initial rotation variable
+        myInitialRotation = transform.rotation;
+
+        // Assign rotations for front, back, left, and right rotation.
+        frontRotation = Quaternion.Euler(0f,0f,0f);
+        backRotation = Quaternion.Euler(0f,180f,0f);
+        leftRotation = Quaternion.Euler(0f,90f,0f);
+        rightRotation = Quaternion.Euler(0f,270f,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Manage Rotation around Y-Axis
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            yRot = -0.5f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            yRot = 0.5f;
+        }
+        else 
+        {
+            yRot = 0.0f;
+        }
+
+        eulerRot = new Vector3(0.0f, yRot, 0.0f);
+        transform.Rotate(eulerRot, Space.Self);  
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = myInitialRotation;
+        Debug.Log("Rotation Reset!");
+    }
+
+    public void RotateToFront()
+    {
+        transform.rotation = frontRotation;
+        Debug.Log("Rotated to Front!");
+    }
+
+    public void RotateToBack()
+    {
+        transform.rotation = backRotation;
+        Debug.Log("Rotated to Back!");
+    }
+    
+    public void RotateToLeft()
+    {
+        transform.rotation = leftRotation;
+        Debug.Log("Rotated to Left!");
+    }
+
+    public void RotateToRight()
+    {
+        transform.rotation = rightRotation;
+        Debug.Log("Rotated to Right!");
+    }
+}
