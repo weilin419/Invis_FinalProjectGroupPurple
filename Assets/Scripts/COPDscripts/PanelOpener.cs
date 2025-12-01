e451b378a21c8162fcc3cf5c08b04f03a48a5f2f
an-neb1 <nebunu.andreea07@gmail.com>
2025-12-01 00:46:25 +0000

    I finished my COPD scene

---
+++ Assets/Scripts/COPDscripts/PanelOpener.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour {
+public GameObject Panel;
+public void OpenPanel()
+
+{
+    if (Panel != null)
+    {
+        bool isActive = Panel.activeSelf;
+        Panel.SetActive(!isActive);
+    }
+}
+}
