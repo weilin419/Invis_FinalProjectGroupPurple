f8673ecc41c1025613af5f63862aa5823a8032c1
an-neb1 <nebunu.andreea07@gmail.com>
2025-12-10 18:29:29 +0000

    Finished the Quiz

--- Assets/Scripts/TEMPLoadSceneAndrea.cs
+++ Assets/Scripts/TEMPLoadSceneAndrea.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//This sript is written by Andreea with Kerry's help
 public class TEMPLoadSceneAndrea : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
